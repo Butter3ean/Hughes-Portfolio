3a47628bf86f517d42607a2e5ace039fae7a99ea
Dalton Hughes <hughesda@lee.k12.ga.us>
2024-02-02 15:31:33 -0500

    Added divs to html for simple pong game along with styles in style.css and started some functions in script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function gameSetup() {
+    ball = document.getElementsByClassName('ball')
+    paddle1 = document.getElementsByClassName('paddle1')
+    paddle2 = document.getElementsByClassName('paddle2')
+
+    while(true) {
+
+    }
+}
+
+function upPressed() {
+
+}
+
+function downPressed() {
+
+}
